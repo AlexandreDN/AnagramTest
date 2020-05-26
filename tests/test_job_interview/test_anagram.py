5a9066ba05cd46803e23b843cc36a90e8d3ce3b4
AlexandreDN <alexandre.das-neves@esme.fr>
2020-05-26 18:30:31 +0200

    test anagram

--- tests/test_job_interview/test_anagram.py
+++ tests/test_job_interview/test_anagram.py
@@ -6,9 +6,21 @@ from esme_lessons.job_interview.anagrams import anagram
 class TestAnagram(unittest.TestCase):
     def test_anagram(self):
         # Given
-        pass
+        n = '68169'
+        expected_output = 98660
         # When
+        output = anagram(n)
 
         # Then
+        self.assertEqual(expected_output, output)
+
+    def test_anagram_negative(self):
+        #Given
+        n = -1
+        expected_output = None
+        #When
+        output = anagram(n)
+        #Then
+        self.assertEqual(expected_output, output)
 
 
